556b925dd3ab0d1e295c9bc1156fc2c1d3d3d54d
Amarjot singh <amarjotsingh90@yahoo.com>
2018-02-16 13:09:03 -0800

    http basic auth fixed

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Create Secure Tunnel to your Localhost with Ngrok
 
 This repository contains the code to connect your local development environment, to publically accessible URL.
+
 It's mainly used to test and implement webhooks in the development stage.
 
+Moreover, you can set up basic http auth for added security.
--- app.js
+++ app.js
@@ -1,23 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const ngrok = require('ngrok');
+const user = process.env.USER;
+const password = process.env.PASSWORD;
 
-
-app.get('/',(req,res)=>{
-    res.send('This is the express tunnel');
+app.get('/', (req, res) => {
+    res.send('This is the tunnel created by Ngrok with Http Auth');
 });
 
-const server = app.listen(5000,()=>{
-    console.log('Express listening at ',server.address().port);
+const server = app.listen(process.env.PORT, () => {
+    console.log('Express listening at ', server.address().port);
 })
 
-ngrok.connect(server.address().port,(err,url)=>{
-    if(err) {
+ngrok.connect({
+    proto : 'http',
+    addr : process.env.PORT,
+    auth : `${user}:${password}`
+}, (err, url) => {
+    if (err) {
         console.err('Error while connecting Ngrok');
         return new Error('Ngrok Failed');
-    }else{
-        console.log('Tunnel Created -> ',url);
+    } else {
+        console.log('Tunnel Created -> ', url);
         console.log('Tunnel Inspector ->  http://127.0.0.1:4040');
     }
 });
